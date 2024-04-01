a4d41949529c5c71eca9093bc89882e75a3865e5
toxiic-me <iamtoxiicman@gmail.com>
2024-04-01 16:45:59 +0530

    all set

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,31 +7,29 @@ const mongoose = require('mongoose');
 
 // global variables
 const PORT = process.env.PORT || 8080;
-const URI = "mongodb+srv://allblogsdata:%40Hanish870807@cluster0.tdruxdp.mongodb.net/pxdNGL?retryWrites=true&w=majority"
-
 // common middlewares
 app.use(cors());
 app.use(express.json());
 
 // function to connect database
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
         console.log('Connecting to Database...');
-        await mongoose.connect(URI)
-        .then(()=>console.log('Connected to Database'))
-        .catch(()=>{
-            console.log('Connection Failed');
-            console.log('Connecting to database in 5 seconds...');
-            setTimeout(() => {
-                connectDB();            
-            }, 5000);
-        })
-        
+        await mongoose.connect(process.env.URI)
+            .then(() => console.log('Connected to Database'))
+            .catch(() => {
+                console.log('Connection Failed');
+                console.log('Connecting to database in 5 seconds...');
+                setTimeout(() => {
+                    connectDB();
+                }, 5000);
+            })
+
     } catch (error) {
         console.log('Try-Catch Failed.');
         console.log('Connecting to database in 5 seconds...');
         setTimeout(() => {
-            connectDB();            
+            connectDB();
         }, 5000);
     }
 }
@@ -47,32 +46,32 @@ const msgSchema = new mongoose.Schema({
     date: String,
     read: Boolean
 })
-const Msgs = new mongoose.model("msgs",msgSchema);
+const Msgs = new mongoose.model("msgs", msgSchema);
 
 // api to save msg
-app.post('/send', async (req,res)=>{
-    console.log('Confession:',req.body.message);
-    console.log("Time:",req.body.date);
+app.post('/send', async (req, res) => {
+    console.log('Confession:', req.body.message);
+    console.log("Time:", req.body.date);
     const response = await Msgs.create(req.body);
-    response ? res.send(true): res.send(false)
+    response ? res.send(true) : res.send(false)
 })
 
 // api to get msg
-app.get('/', async(req,res)=>{
+app.get('/', async (req, res) => {
     const response = await Msgs.find({});
-    response.length !== 0 ? res.send(response): res.send([])
+    response.length !== 0 ? res.send(response) : res.send([])
 })
 
 // api to mark msg as red
-app.post('/markRead', async (req,res)=>{
+app.post('/markRead', async (req, res) => {
     console.log(req.body);
-    const response = await Msgs.updateOne(req.body,{read:true});
+    const response = await Msgs.updateOne(req.body, { read: true });
     console.log(response);
-    response.modifiedCount != 0 ? res.send(true): res.send(false)
+    response.modifiedCount != 0 ? res.send(true) : res.send(false)
 })
 
 
-app.listen(PORT,()=>{
-    console.log('Server live on PORT:',PORT);
+app.listen(PORT, () => {
+    console.log('Server live on PORT:', PORT);
 })
 
